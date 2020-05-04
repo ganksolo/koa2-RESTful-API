b03650b78954e1f96d875b42c33e7b6af452c5df
贾玉龙 <f2engineer@outlook.com>
2020-05-04 20:26:50 +0800

    discarded

--- index.js
+++ index.js
@@ -3,22 +3,43 @@ const app = new Koa();
 
 const KoaRouter = require('koa-router');
 // const api = require('./api/test');
-const router = new KoaRouter();
+const apiRouter = new KoaRouter();
 
-// //注册路由
-router.get('/', (ctx, next) => {
-    // const start = new now();
-    // ctx.body = "hello world"
-    // await next();
-    // const ms = Date.now() - start;
-    // console.log(`${ctx.method}--${ctx.url}--${ms}ms`);
-    console.log('index');
-    ctx.body = 'index';
+apiRouter.prefix('/api');
+
+// 配置get路由
+apiRouter.get('/get', (ctx, next) => {
+    ctx.body = {
+        msg: 'test',
+        query: ctx.query,
+        querystring: ctx.querystring,
+    }
+})
+// 配置post路由
+apiRouter.post('/post', (ctx, next) => {
+    ctx.body = 'this is a post response!'
+})
+
+const adminRouter = new KoaRouter();
+
+adminRouter.get('/', async (ctx, next) => {
+    const start = Date.now();
+    ctx.body = "hello world11"
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method}--${ctx.url}--${ms}ms`);
+})
+
+adminRouter.get('/get', (ctx, next) => {
+    ctx.body = 'this is get res!';
 })
 
 
-// 加载路由中间件
-app.use(router.routes()).use(router.allowedMethods())
+
+// 注册路由中间件
+app.use(adminRouter.routes(), adminRouter.allowedMethods())
+app.use(apiRouter.routes(), apiRouter.allowedMethods())
+
 
 app.listen(3000, () => {
     console.log('[demo] route-use-middleware is starting at port 3000')
