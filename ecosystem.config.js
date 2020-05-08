d80e1866c21a240f00d65226b80269802233b01f
贾玉龙 <f2engineer@outlook.com>
2020-05-08 18:28:31 +0800

    更改pm2部署配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,11 +22,11 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
+      user : 'root',
+      host: '39.106.138.188',
       ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      repo: 'https://github.com/ganksolo/koa2-RESTful-API.git',
+      path: '/www/wwwroot/',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
